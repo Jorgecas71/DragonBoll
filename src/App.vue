8a60f7c705981a282086fb1bd8e42776284d720f
Jorgecas71 <jorgecas71@gmail.com>
2024-06-11 14:47:24 +0200

    init proyect

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/planets">Planets</RouterLink>
+        
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PlanetsView from '../views/PlanetsView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
+    {
+      path: '/',
+      name: 'planets',
+      component: PlanetsView
+    },
   ]
 })
 
